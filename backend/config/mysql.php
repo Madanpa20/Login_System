2f9e2e6108ae25f87e07bdc5d3314bf2ca392686
madanpa20 <madan.pa2024it@sece.ac.in>
2026-01-30 11:28:30 +0530

    Convert backend submodule to regular folder

---
+++ backend/config/mysql.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "mydb";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed");
+}
+
+?>
